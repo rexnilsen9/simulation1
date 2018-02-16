d7f30b73fb0abef397520edaf57ca413a1dbd2ba
rex nilsen <rexpnilsen9@gmail.com>
2018-02-16 15:37:11 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Switch } from 'react-router-dom';
 import routes from './routes'
 
+import './reset.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className='mainHeader'>
-          <h1>Shelfie</h1>
+          <Link id='link' to='/'><h1>
+            <img src="https://github.com/DevMountain/simulation-1/blob/master/assets/logo.png?raw=true" alt=""></img>
+            SHELFIE
+            </h1></Link>
         </header>
-        <div className='linkwrapper'>
-         <Link to='/bins/A'>Shelf A</Link><br/>
-         <Link to='/bins/B'>Shelf B</Link><br/>
-         <Link to='./bins/C'>Shelf C</Link><br/>
-         <Link to='./bins/D'>Shelf D</Link>
-        </div>
-      { routes }
+        <Switch>
+          <div className='linkwrapper'>
+            <Link id='link' to='/bins/A'>Shelf A</Link><br /><br/>
+            <Link id='link' to='/bins/B'>Shelf B</Link><br /><br/>
+            <Link id='link' to='./bins/C'>Shelf C</Link><br /><br/>
+            <Link id='link' to='./bins/D'>Shelf D</Link>
+          </div>
+        </Switch>
+        {routes}
       </div>
     );
   }
